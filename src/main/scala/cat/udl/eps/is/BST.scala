35b8fcb25532f459cdd455a0f6a8e73a69b50585
SaraSerrano7 <sasecu@gmail.com>
2023-03-17 12:58:23 +0100

    fixed insert2, trying fromList

--- src/main/scala/cat/udl/eps/is/BST.scala
+++ src/main/scala/cat/udl/eps/is/BST.scala
@@ -10,7 +10,7 @@ enum BST[+A]:
 
   // La funció que passem ens indica si el primer paràmetre és més petit (<) que el segon
   
-  //se asume que no se insertan elementos repetidos
+  //Esta versión no -> se asume que no se insertan elementos repetidos
   def insert[B >: A](a: B)(lt: (B, B) => Boolean): BST[B] =
     this match
       case Node(left, value, right) => if 
@@ -21,15 +21,14 @@ enum BST[+A]:
   def insert2[B >: A](a: B)(lt: (B, B) => Boolean): BST[B] =
     this match
       case Empty => Node(Empty, a, Empty)
-      case Node(left, value, right) if lt(a, value) => left.insert2(a)(lt)
-      case Node(left, value, right) if lt(value, a) => right.insert2(a)(lt)
+      case Node(left, value, right) if lt(a, value) => Node(left.insert2(a)(lt), value, right)
+      case Node(left, value, right) if lt(value, a) => Node(left, value, right.insert2(a)(lt))
       case _ => this
       
     
 
   // 2.find
 
-  //nota: se podría mejorar el insert usando primero el find, que si da true se retorna this (o con Option)
   def find[B >: A](a: B)(lt: (B, B) => Boolean): Boolean = 
     this match
       case Node(left, value, right) => if lt(a, value)
@@ -41,19 +40,25 @@ enum BST[+A]:
   // 3.fold
 
   // També és un dels que no ho provocat majors problemes
-  def fold[B](b: B)(f: (B, A, B) => B): B = ???
-  /*
+  def fold[B](b: B)(f: (B, A, B) => B): B = 
     this match
-      case Node(left.fold(b)(f), value, right.fold(b)(f)) => f(left, value, right)
+      case Node(left, value, right) => f(left.fold(b)(f), value, right.fold(b)(f))
       case Empty => b
-    */
 
 object BST:
 
   // Les llistes que farem servir són les de scala
 
   // 4. fromList
+  /*
+  Nota: podremos guardar la lista en forma de árbol no completo (o no equilibrado, no recuerdo)
+  porque, despues en inorder para retornar el árbol como lista, al ser estas simple linked list,
+  no afectará al desperdicio de espacio
+  */
   def fromList[A](l: List[A])(lt: (A, A) => Boolean): BST[A] = ???
-
+    /* l match
+      case head :: next => Node(Empty, head, Empty).insert2(next)(lt).fromList(next)(lt)
+      case Nil => Empty
+*/ 
   // 5. inorder (via fold)
   def inorder[A](t: BST[A]): List[A] = ???
--- src/worksheets/bst_worksheet.worksheet.sc
+++ src/worksheets/bst_worksheet.worksheet.sc
@@ -21,3 +21,23 @@ node5.find(7)(_ == _)
 node5.insert(5)(_ < _)
 
 node5.insert2(5)(_ < _)
+
+node5
+
+//----------------
+
+val initial = Empty
+
+val values = List(1, 2, 3, 4)
+
+//Cons(1, (2, 3, 4))
+
+val step1 = initial.insert2(1)(_ < _)
+
+//Cons(2, (3, 4))
+
+val step2 = step1.insert2(2)(_ < _)
+
+//Cons(3, (4))
+
+val step3 = step2.insert2(3)(_ < _)
